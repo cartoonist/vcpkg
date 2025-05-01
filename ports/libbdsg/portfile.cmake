1cace32dcd71a4d91ed6b344ad7cb98d45dd2b6f
Ali Ghaffaari <ali.ghaffaari@uni-bielefeld.de>
2025-05-01 22:07:58 +0200

    Update libbdsg to fix CMake min version

--- ports/libbdsg/portfile.cmake
+++ ports/libbdsg/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO vgteam/libbdsg
-    REF 704567184a3cfacfcb1d51e58be771b01c25ab15
-    SHA512 2e916b30675943dbd72feb9be90b4ac965d2a2b02517d425d35a59d8b76ac4d8135de9d23ca16edf2406ee0f379fb6a19b3fac51a5b723e419c7c9d2bc680869
+    REF dfe53fd2fb8fc9b748b8c24cfc3fb7fbc6e1ee32
+    SHA512 8e061b4e4e0a44c011e653ec0cc4118787044df1ab1019699c58391ea34ce8f2d03366c83bfefc712548b6c17b137edb46ec0c7bb24b41fdefe5ed6a4dd9266e
     HEAD_REF master
     PATCHES
         00-fix-cmake-deps.patch
